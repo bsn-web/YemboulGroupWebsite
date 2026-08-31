760dd0eacd19b9a44399eed434ab54ea915dbd06
Abdelhadi Saidane <57494856+bsn-web@users.noreply.github.com>
2026-08-31 10:33:18 +0100

    Reduce logo count from 68 to 63 in OurClients component

--- src/components/home/OurClients.js
+++ src/components/home/OurClients.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const OurClients = () => {
-  const logos = Array.from({ length: 68 }, (_, i) => {
+  const logos = Array.from({ length: 63 }, (_, i) => {
     const id = i + 1;
-    return `/images/partners/${id}.${id === 1 || id === 10 || id === 16 || id === 68 ? 'jpg' : 'png'}`;
+    return `/images/partners/${id}.${id === 1 || id === 10 || id === 16 || id === 63 ? 'jpg' : 'png'}`;
   });
 
   return (
